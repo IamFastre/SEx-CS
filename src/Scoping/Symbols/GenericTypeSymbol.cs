cc4f38070bbb4ab5d4d404b990f1074d70bbee9c
IamFastre <mahmoudmode057@gmail.com>
2023-12-05 19:20:02 +0200

    More stuff

--- src/Scoping/Symbols/GenericTypeSymbol.cs
+++ src/Scoping/Symbols/GenericTypeSymbol.cs
@@ -1,3 +1,4 @@
+using SEx.Evaluate.Values;
 using SEx.Generic.Constants;
 
 namespace SEx.Scoping;
@@ -5,13 +6,33 @@ namespace SEx.Scoping;
 internal sealed class GenericTypeSymbol : TypeSymbol
 {
     public TypeSymbol[] Parameters { get; private set; }
+
     public override SymbolKind Kind => SymbolKind.GenericType;
 
-    public GenericTypeSymbol(string name, params TypeSymbol[] parameters)
-        : base(name) => Parameters = parameters;
+    public GenericTypeSymbol(string name, ValType typeKind = ValType.Any, TypeSymbol? elementType = null, params TypeSymbol[] parameters)
+        : base(name, typeKind, elementType)
+        => Parameters = parameters;
 
     public void SetParameters(params TypeSymbol[] parameters)
         => Parameters = parameters;
+
+    public override bool Matches(TypeSymbol other)
+    {
+        if (other is GenericTypeSymbol gen && ID.HasFlag(other.ID))
+        {
+            if (Parameters.Length != gen.Parameters.Length)
+                return false;
+
+            for (int i = 0; i < Parameters.Length; i++)
+                if (Parameters[i].ID != gen.Parameters[i].ID)
+                    return false;
+
+            return true;
+        }
+
+        return false;
+    }
+
 
     public override string ToString()
     {
@@ -30,5 +51,5 @@ internal sealed class GenericTypeSymbol : TypeSymbol
     }
 
     public static GenericTypeSymbol List(TypeSymbol type)
-        => new(CONSTS.LIST, type);
+        => new(CONSTS.LIST, ValType.List, type, type);
 }
--- src/Scoping/Symbols/TypeSymbol.cs
+++ src/Scoping/Symbols/TypeSymbol.cs
@@ -1,27 +1,69 @@
+using SEx.Evaluate.Values;
 using SEx.Generic.Constants;
 
 namespace SEx.Scoping;
 
 internal class TypeSymbol : Symbol
 {
+    public ValType     ID          { get; }
+    public TypeSymbol? ElementType { get; }
+
+    public bool IsKnown    => ID is not ValType.Unknown;
+    public bool IsIterable => ElementType is not null;
+
     public override SymbolKind Kind => SymbolKind.Type;
-    protected TypeSymbol(string name) : base(name) { }
+
+    protected TypeSymbol(string name, ValType typeKind = ValType.Any, TypeSymbol? elementType = null)
+        : base(name)
+    {
+        ID          = typeKind;
+        ElementType = elementType;
+    }
 
     public override int  GetHashCode()       => ToString().GetHashCode();
     public override bool Equals(object? obj) => obj?.ToString() == ToString();
 
+
+    public virtual bool Matches(TypeSymbol other)
+        => ID.HasFlag(other.ID);
+
     // Special types
-    public static readonly TypeSymbol Unknown = new(CONSTS.UNKNOWN);
-    public static readonly TypeSymbol Void    = new(CONSTS.VOID);
-    public static readonly TypeSymbol Any     = new(CONSTS.ANY);
+    public static readonly TypeSymbol Unknown = new(CONSTS.UNKNOWN, ValType.Unknown);
+    public static readonly TypeSymbol Void    = new(CONSTS.VOID,    ValType.Void);
+    public static readonly TypeSymbol Nones   = new("nones",        ValType.Nones);
 
     // Data types
-    public static readonly TypeSymbol Null    = new(CONSTS.NULL);
-    public static readonly TypeSymbol Bool    = new(CONSTS.BOOLEAN);
-    public static readonly TypeSymbol Number  = new(CONSTS.NUMBER);
-    public static readonly TypeSymbol Int     = new(CONSTS.INTEGER);
-    public static readonly TypeSymbol Float   = new(CONSTS.FLOAT);
-    public static readonly TypeSymbol Char    = new(CONSTS.CHAR);
-    public static readonly TypeSymbol String  = new(CONSTS.STRING);
-    public static readonly TypeSymbol Range   = new(CONSTS.RANGE);
+    public static readonly TypeSymbol Any     = new(CONSTS.ANY,     ValType.Any);
+    public static readonly TypeSymbol Null    = new(CONSTS.NULL,    ValType.Null);
+    public static readonly TypeSymbol Bool    = new(CONSTS.BOOLEAN, ValType.Boolean);
+    public static readonly TypeSymbol Number  = new(CONSTS.NUMBER,  ValType.Number);
+    public static readonly TypeSymbol Int     = new(CONSTS.INTEGER, ValType.Integer);
+    public static readonly TypeSymbol Float   = new(CONSTS.FLOAT,   ValType.Float);
+    public static readonly TypeSymbol Char    = new(CONSTS.CHAR,    ValType.Char);
+    public static readonly TypeSymbol String  = new(CONSTS.STRING,  ValType.String, Char);
+    public static readonly TypeSymbol Range   = new(CONSTS.RANGE,   ValType.Range,  Number);
+}
+
+internal static class TypeExtension
+{
+    public static bool Match(this (TypeSymbol L, TypeSymbol R) Ns, TypeSymbol left, TypeSymbol? right = null, bool interchangeable = false)
+    {
+       if (right is null)
+            return left.Matches(Ns.L) && left.Matches(Ns.R);
+
+        if (interchangeable)
+            return left.Matches(Ns.L) && right.Matches(Ns.R) || left.Matches(Ns.R) && right.Matches(Ns.L);
+        return left.Matches(Ns.L) && right.Matches(Ns.R);
+    }
+
+    public static bool IsAssignable(this (TypeSymbol hint, TypeSymbol value) types, bool interchangeable = false)
+    {
+        if (interchangeable)
+            return types.hint.Matches(types.value)
+                || types.value.Matches(types.hint);
+
+        return types.hint.Matches(types.value)
+            || TypeSymbol.Nones.Matches(types.value)
+            || TypeSymbol.Nones.Matches(types.hint);
+    }
 }
