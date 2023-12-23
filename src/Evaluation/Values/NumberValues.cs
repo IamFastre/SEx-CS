19e95651f2f0bc4841ef99b851c4d54f1e8589f2
IamFastre <mahmoudmode057@gmail.com>
2023-12-23 18:45:20 +0200

    Fixed a problem with C#'s negative zeros

--- src/Evaluation/Values/NumberValues.cs
+++ src/Evaluation/Values/NumberValues.cs
@@ -20,6 +20,8 @@ public abstract class NumberValue : LiteralValue
 
     public override int GetHashCode() => ((double) Value).GetHashCode();
 
+    protected static double Prepare(double value)
+        => double.NegativeZero == value ? 0D : value;
 }
 
 public sealed class IntegerValue : NumberValue
@@ -28,7 +30,7 @@ public sealed class IntegerValue : NumberValue
     public  override object Value => _value;
     public  override TypeSymbol Type => TypeSymbol.Integer;
 
-    public IntegerValue(double value) => _value = Math.Floor(value);
+    public IntegerValue(double value) => _value = Math.Floor(Prepare(value));
 
     public static bool IsInt(double value)
         => double.IsInteger(value) || double.IsInfinity(value)|| double.IsNaN(value);
@@ -43,10 +45,10 @@ public sealed class IntegerValue : NumberValue
 public sealed class FloatValue : NumberValue
 {
     private readonly double _value;
-    public  override object Value => _value;
-    public  override TypeSymbol Type => TypeSymbol.Float;
+    public override object Value => _value;
+    public override TypeSymbol Type => TypeSymbol.Float;
 
-    public FloatValue(double value) => _value = value;
+    public FloatValue(double value) => _value = Prepare(value);
 
     public override string ToString()
         => C.YELLOW2 + GetString() + C.END;
