fcdae04d77cfc42ecb43d892ece1f02300268bc6
IamFastre <mahmoudmode057@gmail.com>
2024-01-19 07:05:58 +0200

    added automation for publishing in batch script

---
+++ publish.cmd
@@ -0,0 +1,19 @@
+@echo off
+
+CALL :PUBLISH "Windows" "win-x64"
+CALL :PUBLISH "Linux" "linux-x64"
+@REM CALL :PUBLISH "macOS" "osx-x64"
+
+EXIT /B 0
+
+:PUBLISH
+    echo [*] Building app for '%~1'...
+    dotnet publish %~1.csproj
+    echo [*] Done!
+    echo [*] Moving files...
+    mkdir "build\%~1"
+    rmdir "build\%~1\*" /s /q
+    move "build\Debug\%~1\publish\*" "build\%~1\"
+    rmdir "build\Debug\%~1\publish" /s /q
+    echo [*] Done!
+    echo.
