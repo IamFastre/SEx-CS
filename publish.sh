57b78a76a8bcd1d3f3d5962390832bf48a55b91a
IamFastre <mahmoudmode057@gmail.com>
2024-01-19 07:05:53 +0200

    added automation for publishing in shell script

---
+++ publish.sh
@@ -0,0 +1,15 @@
+publish() {
+    echo "[*] Building app for '$1'..."
+    dotnet publish $1.csproj
+    echo "[*] Done!"
+    echo "[*] Moving files..."
+    rm "./build/$1" -r -v
+    mv "./build/Debug/$2/publish" "./build/$1" -v
+    rm "./build/Debug/$2" -r -v
+    echo "[*] Done!"
+    echo
+}
+
+publish "Linux" "linux-x64"
+publish "Windows" "win-x64"
+# publish "macOS" "osx-x64"
