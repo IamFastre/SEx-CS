06df3266bf50fae6b491fe68a6597d9533740108
IamFastre <mahmoudmode057@gmail.com>
2024-01-19 22:08:09 +0200

    To be consistent with the windows file

--- publish.sh
+++ publish.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 publish() {
-    echo "[*] Building app for '$1'..."
+    echo "[*] Building app for $1..."
     dotnet publish $1.csproj
     echo "[*] Done!"
     echo "[*] Moving files..."
-    rm "./build/$1" -r -v
+    rm "./build/$1" -r
+    mkdir "./build/$1"
     mv "./build/Debug/$2/publish" "./build/$1" -v
     rm "./build/Debug/$2" -r -v
     echo "[*] Done!"
