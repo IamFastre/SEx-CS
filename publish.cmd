6bda31f94a8a888bef94d4f3d1016bfd3f02dcc7
IamFastre <mahmoudmode057@gmail.com>
2024-01-19 22:07:50 +0200

    Big wrong tsk tsk tsk

--- publish.cmd
+++ publish.cmd
@@ -7,13 +7,13 @@ CALL :PUBLISH "Linux" "linux-x64"
 EXIT /B 0
 
 :PUBLISH
-    echo [*] Building app for '%~1'...
+    echo [*] Building app for %~1...
     dotnet publish %~1.csproj
     echo [*] Done!
     echo [*] Moving files...
     mkdir "build\%~1"
-    rmdir "build\%~1\*" /s /q
-    move "build\Debug\%~1\publish\*" "build\%~1\"
-    rmdir "build\Debug\%~1\publish" /s /q
+    del   "build\%~1\*" /s /q /f
+    move  "build\Debug\%~2\publish\*" "build\%~1\"
+    rmdir "build\Debug\%~2\" /s /q
     echo [*] Done!
     echo.
