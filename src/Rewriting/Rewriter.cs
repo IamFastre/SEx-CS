09cb8790c8a28c26bd6f756c0e834126c5faae60
IamFastre <mahmoudmode057@gmail.com>
2024-01-25 08:08:18 +0200

    More to rewriters I think those methods need to be virtual not abstract but whatever

--- src/Rewriting/Rewriter.cs
+++ src/Rewriting/Rewriter.cs
@@ -7,7 +7,9 @@ public abstract class Rewriter
     public SemanticProgramStatement Rewrite(SemanticProgramStatement program)
         => new(program.Body.Select(RewriteStatement).ToArray());
 
-
+    /* ====================================================================== */
+    /*                               Statements                               */
+    /* ====================================================================== */
     public SemanticStatement RewriteStatement(SemanticStatement stmt) => stmt switch
     {
         SemanticBlockStatement       blkStmt   => RewriteBlockStatement(blkStmt),
@@ -30,4 +32,37 @@ public abstract class Rewriter
     public abstract SemanticForStatement         RewriteForStatement(SemanticForStatement forStmt);
     public abstract SemanticFunctionStatement    RewriteFunctionStatement(SemanticFunctionStatement funcStmt);
     public abstract SemanticReturnStatement      RewriteReturnStatement(SemanticReturnStatement retStmt);
+
+
+    /* ====================================================================== */
+    /*                               Expressions                              */
+    /* ====================================================================== */
+    public SemanticExpression RewriteExpression(SemanticExpression expr) => expr switch
+    {
+        SemanticAssignment          asExpr   => RewriteAssignment(asExpr),
+        SemanticIndexAssignment     inAsExpr => RewriteIndexAssignment(inAsExpr), // "in ass expression" lol
+        SemanticCallExpression      callExpr => RewriteCallExpression(callExpr),
+        SemanticIndexingExpression  inExpr   => RewriteIndexingExpression(inExpr),
+        SemanticFailedExpression    failExpr => RewriteFailedExpression(failExpr),
+        SemanticUnaryOperation      uOp      => RewriteUnaryOperation(uOp),
+        SemanticCountingOperation   cOp      => RewriteCountingOperation(cOp),
+        SemanticBinaryOperation     biOp     => RewriteBinaryOperation(biOp),
+        SemanticTernaryOperation    terOp    => RewriteTernaryOperation(terOp),
+        SemanticFailedOperation     failOp   => RewriteFailedOperation(failOp),
+        SemanticConversionOperation conOp    => RewriteConversionOperation(conOp),
+
+        _ => throw new Exception("Expression unadded")
+    };
+
+    public abstract SemanticAssignment          RewriteAssignment(SemanticAssignment asExpr);
+    public abstract SemanticIndexAssignment     RewriteIndexAssignment(SemanticIndexAssignment inAsExpr);
+    public abstract SemanticCallExpression      RewriteCallExpression(SemanticCallExpression callExpr);
+    public abstract SemanticIndexingExpression  RewriteIndexingExpression(SemanticIndexingExpression inExpr);
+    public abstract SemanticFailedExpression    RewriteFailedExpression(SemanticFailedExpression failExpr);
+    public abstract SemanticUnaryOperation      RewriteUnaryOperation(SemanticUnaryOperation uOp);
+    public abstract SemanticCountingOperation   RewriteCountingOperation(SemanticCountingOperation cOp);
+    public abstract SemanticBinaryOperation     RewriteBinaryOperation(SemanticBinaryOperation biOp);
+    public abstract SemanticTernaryOperation    RewriteTernaryOperation(SemanticTernaryOperation terOp);
+    public abstract SemanticFailedOperation     RewriteFailedOperation(SemanticFailedOperation failOp);
+    public abstract SemanticConversionOperation RewriteConversionOperation(SemanticConversionOperation conOp);
 }
