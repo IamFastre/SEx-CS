f8209548e94b2178f767ed2feb81885bbeb9f0ec
IamFastre <mahmoudmode057@gmail.com>
2024-01-25 07:30:24 +0200

    Added script rewriters

---
+++ src/Rewriting/Rewriter.cs
@@ -0,0 +1,33 @@
+using SEx.Semantics;
+
+namespace SEx.Rewriting;
+
+public abstract class Rewriter
+{
+    public SemanticProgramStatement Rewrite(SemanticProgramStatement program)
+        => new(program.Body.Select(RewriteStatement).ToArray());
+
+
+    public SemanticStatement RewriteStatement(SemanticStatement stmt) => stmt switch
+    {
+        SemanticBlockStatement       blkStmt   => RewriteBlockStatement(blkStmt),
+        SemanticExpressionStatement  exprStmt  => RewriteExpressionStatement(exprStmt),
+        SemanticDeclarationStatement decStmt   => RewriteDeclarationStatement(decStmt),
+        SemanticIfStatement          ifStmt    => RewriteIfStatement(ifStmt),
+        SemanticWhileStatement       whileStmt => RewriteWhileStatement(whileStmt),
+        SemanticForStatement         forStmt   => RewriteForStatement(forStmt),
+        SemanticFunctionStatement    funcStmt  => RewriteFunctionStatement(funcStmt),
+        SemanticReturnStatement      retStmt   => RewriteReturnStatement(retStmt),
+
+        _ => throw new Exception("Statement unadded")
+    };
+
+    public abstract SemanticBlockStatement       RewriteBlockStatement(SemanticBlockStatement blkStmt);
+    public abstract SemanticExpressionStatement  RewriteExpressionStatement(SemanticExpressionStatement exprStmt);
+    public abstract SemanticDeclarationStatement RewriteDeclarationStatement(SemanticDeclarationStatement decStmt);
+    public abstract SemanticIfStatement          RewriteIfStatement(SemanticIfStatement ifStmt);
+    public abstract SemanticWhileStatement       RewriteWhileStatement(SemanticWhileStatement whileStmt);
+    public abstract SemanticForStatement         RewriteForStatement(SemanticForStatement forStmt);
+    public abstract SemanticFunctionStatement    RewriteFunctionStatement(SemanticFunctionStatement funcStmt);
+    public abstract SemanticReturnStatement      RewriteReturnStatement(SemanticReturnStatement retStmt);
+}
