0e70cf71a278bc7d3e1b3c4f6cd01a7cb4042a68
IamFastre <mahmoudmode057@gmail.com>
2024-02-01 06:47:30 +0200

    publish scripts are much prettier!! ✨ try "publish -h"

--- publish.cmd
+++ publish.cmd
@@ -1,19 +1,47 @@
 @echo off
 
-CALL :PUBLISH "Windows" "win-x64"
-CALL :PUBLISH "Linux" "linux-x64"
-@REM CALL :PUBLISH "macOS" "osx-x64"
+IF "%*"=="" GOTO :ALL
 
-EXIT /B 0
+:START
+    IF "%1"=="-w" CALL :PUBLISH "Windows" "win-x64"
+    IF "%1"=="-l" CALL :PUBLISH "Linux"   "linux-x64"
+    IF "%1"=="-m" CALL :PUBLISH "macOS"   "osx-x64"
+    IF "%1"=="-h" GOTO :HELP
+    IF "%1"=="--help" GOTO :HELP
+    SHIFT
+    IF [%1]==[]   GOTO :EOF
+    GOTO :START
+
+:ALL
+    CALL :PUBLISH "Windows" "win-x64"
+    CALL :PUBLISH "Linux"   "linux-x64"
+    CALL :PUBLISH "macOS"   "osx-x64"
+    GOTO :EOF
 
 :PUBLISH
     echo [*] Building app for %~1...
     dotnet publish %~1.csproj
     echo [*] Done!
+
     echo [*] Moving files...
     mkdir "build\%~1"
     del   "build\%~1\*" /s /q /f
     move  "build\Debug\%~2\publish\*" "build\%~1\"
     rmdir "build\Debug\%~2\" /s /q
     echo [*] Done!
+
     echo.
+
+:HELP
+    echo.
+    echo [*] Publishes the app for all platforms unless specified.
+    echo.
+    echo [*] Usage: %~nx0
+    echo [*] Usage: %~nx0 [options]
+    echo.
+    echo [*] Options:
+    echo       -h, --help      Show this menu
+    echo       -w              Publish app for windows
+    echo       -l              Publish app for linux
+    echo       -m              Publish app for macOS/OSX
+    EXIT /B 0
--- publish.sh
+++ publish.sh
@@ -1,20 +1,55 @@
 #!/bin/sh
 
-publish() {
-    echo "[*] Building app for $1..."
-    dotnet publish $1.csproj
-    echo "[*] Done!"
-    echo "[*] Moving files..."
-    rm "./build/$1" -r
-    mkdir "./build/$1"
-    mv "./build/Debug/$2/publish" "./build/$1" -v
-    rm "./build/Debug/$2" -r -v
-    echo "[*] Done!"
-    echo
+help() {
+  echo
+  echo "[*] Publishes the app for all platforms unless specified."
+  echo
+  echo "[*] Usage: $(basename $0)"
+  echo "[*] Usage: $(basename $0) [options]"
+  echo
+  echo "[*] Options:"
+  echo "      -h, --help      Show this menu"
+  echo "      -w              Publish app for windows"
+  echo "      -l              Publish app for linux"
+  echo "      -m              Publish app for macOS/OSX"
+  exit 0
 }
 
-publish "Linux" "linux-x64"
-publish "Windows" "win-x64"
-# publish "macOS" "osx-x64"
+publish() {
+  echo  "[*] Building app for $1..."
+  dotnet publish $1.csproj
+  echo  "[*] Done!"
 
-exit 0
+  echo  "[*] Moving files..."
+  rm    "./build/$1" -r
+  mkdir "./build/$1"
+  mv    "./build/Debug/$2/publish/" "./build/$1" -v
+  rm    "./build/Debug/$2" -r -v
+  echo  "[*] Done!"
+
+  echo
+}
+
+all() {
+  publish "Linux"   "linux-x64"
+  publish "macOS"   "osx-x64"
+  publish "Windows" "win-x64"
+}
+
+if [ "$*" = "" ];
+  then all
+else
+  for i; do
+    if   [ "$i" = "-l" ];
+      then publish "Linux"   "linux-x64"
+    elif [ "$i" = "-m" ];
+      then publish "macOS"   "osx-x64"
+    elif [ "$i" = "-w" ];
+      then publish "Windows" "win-x64"
+    elif [ "$i" = "-h" ];
+      then help
+    elif [ "$i" = "--help" ];
+      then help
+    fi
+  done
+fi
